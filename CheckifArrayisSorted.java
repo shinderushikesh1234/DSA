65a1045f33a7fd380bc45e60f613caa0e42b488c
Rushikesh tanaji shinde <shinderushikesh7108@gmail.com>
2026-01-04 20:40:50 +0530

    next update

--- CheckifArrayisSorted.java
+++ CheckifArrayisSorted.java
@@ -1,6 +1,6 @@
 class CheckSorted {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4};
+        int[] arr = {25, 35, 60, 42};
 
         boolean isSorted = true;
         for (int i = 0; i < arr.length - 1; i++) {
