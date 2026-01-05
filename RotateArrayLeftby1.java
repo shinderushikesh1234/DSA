6e6e1e8e6d186069dde38dd2f7446d80a76a74c9
Rushikesh tanaji shinde <shinderushikesh7108@gmail.com>
2026-01-05 21:52:19 +0530

    new

--- RotateArrayLeftby1.java
+++ RotateArrayLeftby1.java
@@ -1,6 +1,6 @@
 class RotateLeftByOne {
     public static void main(String[] args) {
-        int[] arr = {10, 25, 35, 44, 59};
+        int[] arr = {1, 2, 3, 4, 5};
 
         int first = arr[0];
         for (int i = 0; i < arr.length - 1; i++) {
