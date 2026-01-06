8730728f335d4d9ecf8bacca324c431ae2ac0a9a
Rushikesh tanaji shinde <shinderushikesh7108@gmail.com>
2026-01-06 21:46:25 +0530

    Updated

--- MinimumElement.java
+++ MinimumElement.java
@@ -1,7 +1,8 @@
 class MinElement {
     public static void main(String[] args) {
-        int[] arr = {4, 2, 9, 1};
-        int min = arr[0];
+        int[] arr1 = {4, 2, 9, 1};
+        int[] arr = arr1;
+        int min = arr1[0];
 
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] < min) min = arr[i];
